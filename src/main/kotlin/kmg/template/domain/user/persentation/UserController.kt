257b310405444f6b693f6f5ecda0079f830370d9
kmg <legolove08@naver.com>
2025-04-02 16:03:57 +0900

    fix: endpoints

--- src/main/kotlin/kmg/template/domain/user/persentation/UserController.kt
+++ src/main/kotlin/kmg/template/domain/user/persentation/UserController.kt
@@ -6,9 +6,11 @@ import kmg.template.domain.user.data.UserRequest
 import kmg.template.domain.user.service.UserService
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
-@RestController("/user")
+@RestController
+@RequestMapping("/user")
 class UserController(
     private val userService: UserService,
 ) {
